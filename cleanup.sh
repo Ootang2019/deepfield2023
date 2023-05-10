8a26926af8dd50606e29f7c3592a9d091bfcfe36
Yu-Tang <yutang.liu@tuebingen.mpg.de>
2023-05-10 18:26:02 +0200

    update

--- cleanup.sh
+++ cleanup.sh
@@ -2,16 +2,11 @@
 
 # stopping all rosnodes
 rosnode kill --all
-# stopping the gazebo client aka gui
-killall gzclient
-# stopping the gazebo server
-killall gzserver
 killall -9 roslaunch
 killall -9 roslaunch
 killall -9 roslaunch
-killall ssd_server.bin
-killall ssd_server.sh
+killall rosmaster
 
-# lets get a bit more drastic
-pkill -f ros/indigo
-pkill -f /home/dementor/catkin_ws
+pkill -9 -f rl.py
+
+
